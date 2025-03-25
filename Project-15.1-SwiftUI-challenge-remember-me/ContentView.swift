d5017fef98ca51e56d165df9a9ccdfab456753aa
kevin cuadros <kevindavidcuadros@gmail.com>
2025-03-25 17:02:50 -0500

    Initial Commit

---
+++ Project-15.1-SwiftUI-challenge-remember-me/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project-15.1-SwiftUI-challenge-remember-me
+//
+//  Created by Kevin Cuadros on 25/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project-15.1-SwiftUI-challenge-remember-me/Project_15_1_SwiftUI_challenge_remember_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_15_1_SwiftUI_challenge_remember_meApp.swift
+//  Project-15.1-SwiftUI-challenge-remember-me
+//
+//  Created by Kevin Cuadros on 25/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Project_15_1_SwiftUI_challenge_remember_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
